0467512132803155701ee83dcb4ceb905a6e50c9
landed-ikar <114181062+landed-ikar@users.noreply.github.com>
2022-09-30 19:34:18 +0300

    Update ProductCategorySelectQuery.sql

--- SQLExample/ProductCategorySelectQuery.sql
+++ SQLExample/ProductCategorySelectQuery.sql
@@ -1,4 +1,6 @@
-select Products.ProductName, Categories.CategoryName
-from Products
-left outer join ProductCategory on Products.ProductID = ProductCategory.ProductID
-left outer join Categories on Categories.CategoryID = ProductCategory.CategoryID
+         select Products.ProductName, Categories.CategoryName
+           from Products
+left outer join ProductCategory 
+             on Products.ProductID = ProductCategory.ProductID
+left outer join Categories 
+             on Categories.CategoryID = ProductCategory.CategoryID

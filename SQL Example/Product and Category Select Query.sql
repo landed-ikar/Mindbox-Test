5e7db586fe5896f3b7c17e630f103cc22c41dcf6
landed-ikar <114181062+landed-ikar@users.noreply.github.com>
2022-09-30 07:58:55 +0300

    Add the answer on the SQL task

---
+++ SQL Example/Product and Category Select Query.sql
@@ -0,0 +1,4 @@
+select Products.ProductName, Categories.CategoryName
+from Products
+left outer join ProductCategory on Products.ProductID = ProductCategory.ProductID
+left outer join Categories on Categories.CategoryID = ProductCategory.CategoryID

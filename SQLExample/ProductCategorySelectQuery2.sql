e8e7dcd1fffe7b12336c2b266581848541a0e290
landed-ikar <114181062+landed-ikar@users.noreply.github.com>
2022-09-30 19:35:20 +0300

    Update ProductCategorySelectQuery2.sql

--- SQLExample/ProductCategorySelectQuery2.sql
+++ SQLExample/ProductCategorySelectQuery2.sql
@@ -1,9 +1,9 @@
-(select Products.ProductName, Categories.CategoryName
-from Products
+        (select Products.ProductName, Categories.CategoryName
+           from Products
 left outer join ProductCategory on Products.ProductID = ProductCategory.ProductID
 left outer join Categories on Categories.CategoryID = ProductCategory.CategoryID)
-union
-(select Null, Categories.CategoryName
-from Categories
+          union
+        (select Null, Categories.CategoryName
+           from Categories
 left outer join ProductCategory on Categories.CategoryID = ProductCategory.CategoryID
-Where ProductCategory.ProductID is null)
+          Where ProductCategory.ProductID is null)
